791b7e8a99dc45a98afd2281ce7b8642f72ae386
zadvajr <danielzadva@gmail.com>
2026-01-18 19:25:32 +0100

    added 02_type_hinting-data_struct2.py

---
+++ advance_type_hints/02_type_hinting_data_struct2.py
@@ -0,0 +1,9 @@
+"""
+Docstring for advance_type_hints.02_type_hinting_data_struct2
+"""
+new_price: list[int] = [14, 902, 898]
+new_immutable_price: tuple[int, int, int] = (388, 392, 299)
+new_price_dict: dict[str, int] = {
+    "item_1": 240,
+    "item_2": 490
+}

c4678d392c528fd6518daf361689a07cefb7ee6a
zadvajr <danielzadva@gmail.com>
2026-01-18 18:34:11 +0100

    added a function without type hints

--- type_hints/01_type_hints.py
+++ type_hints/01_type_hints.py
@@ -4,5 +4,3 @@ print(type(count)) # Output: <class 'int'>
 
 count = '5'
 print(type(count)) # Output <class 'str'>
-
-
---
+++ type_hints/02_func_without_type_hints.py
@@ -0,0 +1,5 @@
+"""Function without type hints"""
+def price_total(price_1, price_2):
+    return f"Your total bill is USD {price_1 + price_2}"
+
+print(price_total(30,40))
